04ef2c817d74ce83082b965c7eb0faf6bdd12062
myj4119 <114908104+myj4119@users.noreply.github.com>
2024-10-23 12:28:12 +0900

    Create data.tf

---
+++ atlantis/data.tf
@@ -0,0 +1,20 @@
+data "aws_ami" "amzl2" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-2.0.*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name = "architecture"
+    values = ["x86_64"]
+  }
+
+  owners = ["137112412989"] # amazon
+}
